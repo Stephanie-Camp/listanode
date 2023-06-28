99ecad3a5af211c0e1112bcd129341ac6b20a75f
Stephanie-Camp <118989596+Stephanie-Camp@users.noreply.github.com>
2023-06-28 19:59:11 +0000

    att

--- jogoPPT.js
+++ jogoPPT.js
@@ -1,13 +1,8 @@
 //jogo Pedra-Papel-Tesoura: usuario vs maquina 
-const readline = require('readline');
-const rl = readline.createInterface(process.stdin);
+const input = require("synchro-prompt");
 let random = Math.floor(Math.random()*3)+1;
+var op = Number(input("Seja bem vindo(a)! Vamos jogar? \n1 - PEDRA \n2 - PAPEL \n3 - TESOURA \nEscolha uma opcao: "));
 
-console.log("Seja bem vindo(a)! Vamos jogar? \Escolha uma opcao: \n1 - PEDRA \n2 - PAPEL \n3 - TESOURA");
-
-rl.question("", (data)=>{
-  var op = Number(data);
-  
   if (op === random){
     console.log(`IIH... Parece que temos um empate! \nMas nao desista, que tal tentar novamente?`);
   }else if (op < random && op > 0){    
@@ -37,5 +32,3 @@ rl.question("", (data)=>{
   }else{
     console.log(`Opcao invalida! \nNao vale usar truques, hein? \Vamos tentar novamente? :)`);
   }
- 
-})
--- numPrimo.js
+++ numPrimo.js
@@ -2,6 +2,10 @@
 const input = require("synchro-prompt");
 var x = Number(input("Digite um numero: "));
 
+if(x === 1 || x < 1){
+  console.log(`${x} nao e um numero primo`);
+}
+
 for(var i = 2; i <= x; i++){
   if((i > 1 && i != x) || x === 1){
     if(x%i === 0){
