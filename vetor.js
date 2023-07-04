0baf1e3320ea2b0dc8ab7a957a54550a6bc5195c
Stephanie-Camp <118989596+Stephanie-Camp@users.noreply.github.com>
2023-07-04 03:36:49 +0000

    att

--- vetor.js
+++ vetor.js
@@ -1,14 +1,14 @@
 //retirar todos os valores '2' do vetor
 const input = require("synchro-prompt");
 var x = Number(input("Defina o tamanho do vetor: "));
-var v = [x]
+var v = [x];
 var vet = [];
 
 console.log(`Digite ${x} valores para inserirmos no vetor: \n`)
 for(var i = 0; i < x; i++){
-  v.push(Number(input(v[i]))); 
+  v.push(Number(input(v[i])));
 }
-v.splice(0, 1); 
+v.splice(0, 1);
 console.log(`Seu vetor: ${v}`);
 
 for(var i = 0; i < x; i++){
@@ -16,5 +16,5 @@ for(var i = 0; i < x; i++){
     vet.push(v[i]);
   }
 }
-console.log()
-console.log(`Vetor = [${vet}]`); 
+console.log();
+console.log(`Vetor = [${vet}]`);
