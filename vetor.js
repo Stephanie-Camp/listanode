1fe478507b0c295ec1447cb3c74971550fc0733a
Stephanie-Camp <118989596+Stephanie-Camp@users.noreply.github.com>
2023-07-05 23:00:30 +0000

    att

--- vetor.js
+++ vetor.js
@@ -1,11 +1,11 @@
-//retirar todos os valores '2' do vetor
+//retirar todos os valores '=num' do vetor
 const input = require("synchro-prompt");
 
 do{
 var x = Number(input("Defina o tamanho do vetor: "));
 var v = [x];
 var vet = [];
-  
+
 //adicione valores dentro do vetor
 console.log(`Digite ${x} valores para inserirmos no vetor: \n`);
 for(var i = 0; i < x; i++){
@@ -14,7 +14,7 @@ for(var i = 0; i < x; i++){
 v.splice(0, 1);
 console.log(`\nSeu vetor: [${v}]`);
 
-//confere os valores dentro do vetor e filtra caso sejam igual a num
+//confere os valores dentro do vetor e remove caso sejam igual a num
 var num = Number(input("\nDigite o valor voce deseja filtrar no seu vetor: "));
 for(var i = 0; i < x; i++){
   if(v[i] != num){
