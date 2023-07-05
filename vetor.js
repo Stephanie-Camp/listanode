cbecf4f5e363f61066a178719c4ceebd98ae6a4f
Stephanie-Camp <118989596+Stephanie-Camp@users.noreply.github.com>
2023-07-05 22:21:44 +0000

    att

--- vetor.js
+++ vetor.js
@@ -1,20 +1,29 @@
 //retirar todos os valores '2' do vetor
 const input = require("synchro-prompt");
+
+do{
 var x = Number(input("Defina o tamanho do vetor: "));
 var v = [x];
 var vet = [];
-
-console.log(`Digite ${x} valores para inserirmos no vetor: \n`)
+  
+//adicione valores dentro do vetor
+console.log(`Digite ${x} valores para inserirmos no vetor: \n`);
 for(var i = 0; i < x; i++){
-  v.push(Number(input(v[i])));
+  v.push((input(v[i] + "\n")));
 }
 v.splice(0, 1);
-console.log(`Seu vetor: ${v}`);
+console.log(`\nSeu vetor: [${v}]`);
 
+//confere os valores dentro do vetor e filtra caso sejam igual a num
+var num = Number(input("\nDigite o valor voce deseja filtrar no seu vetor: "));
 for(var i = 0; i < x; i++){
-  if(v[i] != 2){
+  if(v[i] != num){
     vet.push(v[i]);
   }
 }
-console.log();
-console.log(`Vetor = [${vet}]`);
+console.log(`Vetor final = [${vet}]`);
+
+var op = Number(input("\nAperte qualquer tecla caso queira continuar ou aperte  0  para sair. \nDeseja continuar?\n"));
+}while(op != 0);
+
+console.log("\nVoce saiu.")
