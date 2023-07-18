3aba1fda81fde813b25f7ebb48a6e024faa4bae7
Stephanie-Camp <118989596+Stephanie-Camp@users.noreply.github.com>
2023-07-18 19:23:32 +0000

    att

--- controleCinema.js
+++ controleCinema.js
@@ -1,24 +1,26 @@
 const input = require('synchro-prompt');
-var vet = ["X", ["X", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], ["X", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10] ];
-var op, a, b;
+var vet = [ [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10] ];
+var a, b;
 var v = [];
 
 for(var i = 1; i < vet.length; i++){
 
   while(v.length != 20){  
     var op = Number(input(`\n  Seja bem vindo(a)! Veja aqui os assentos disponiveis: 
-  [${vet[1][1]}]  [${vet[1][2]}]  [${vet[1][3]}]  [${vet[1][4]}]  [${vet[1][5]}]  [${vet[1][6]}]  [${vet[1][7]}]  [${vet[1][8]}]  [${vet[1][9]}]  [${vet[1][10]}]   ---------   FIRST CLASS
+  [${vet[0][1]}]  [${vet[0][2]}]  [${vet[0][3]}]  [${vet[0][4]}]  [${vet[0][5]}]  [${vet[0][6]}]  [${vet[0][7]}]  [${vet[0][8]}]  [${vet[0][9]}]  [${vet[0][10]}]   ---------   FIRST CLASS
              
-  [${vet[2][1]}]  [${vet[2][2]}]  [${vet[2][3]}]  [${vet[2][4]}]  [${vet[2][5]}]  [${vet[2][6]}]  [${vet[2][7]}]  [${vet[2][8]}]  [${vet[2][9]}]  [${vet[2][10]}]   ---------   NORMAL CLASS
+  [${vet[1][1]}]  [${vet[1][2]}]  [${vet[1][3]}]  [${vet[1][4]}]  [${vet[1][5]}]  [${vet[1][6]}]  [${vet[1][7]}]  [${vet[1][8]}]  [${vet[1][9]}]  [${vet[1][10]}]   ---------   NORMAL CLASS
 
   Tipos de assento:
   1 - ASSENTO FRONTAL (PRIMEIRA CLASSE)
   2 - ASSENTO AO FUNDO (NORMAL)
   
   Digite aqui o numero correspondente a fileira que voce prefere se sentar: `));
-
+    
+    op = --op;
+    
     switch (op) {
-      case 1:
+      case 0:
         a = Number(input("  Informe o numero do assento que deseja obter: "));
         if (vet[op][a] === "X") {
           console.log("\nDesculpe, esse assento nao se encontra disponivel! \nSelecione outro, por favor.");
@@ -32,7 +34,7 @@ for(var i = 1; i < vet.length; i++){
         vet[op][a] = "X";
         break;
 
-      case 2:
+      case 1:
         b = Number(input("  Informe o numero do assento que deseja obter: "));
         if (vet[op][b] === "X") {
           console.log("\nDesculpe, esse assento nao se encontra disponivel! \nSelecione outro, por favor.");
